03064b86badb22e9dec84f0427922efe0fefe903
achoi@ca.ibm.com <a24choi@uwaterloo.ca>
2016-07-29 16:50:32 -0400

    M

---
+++ FoodTracker/Sample.swift
@@ -0,0 +1,12 @@
+
+//import Foundation
+import UIKit
+
+
+class SampleController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var welcomeToIBM: UILabel!
+    @IBOutlet weak var GuestCheckIn: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+// MARK: Properties
+}
